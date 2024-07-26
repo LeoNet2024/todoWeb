56d6528f291a7c02019b39a6d4aa90af2bf3ab32
Leon <162373739+LeoNet2024@users.noreply.github.com>
2024-07-26 11:47:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,31 +5,46 @@ let todolist = [
         task: "Clean the house",
         startDate: "17-02-1996",
         endDate: "17-03-1996",
-        status: "not done",
     },
     {
         task: "Java homework",
         startDate: "22-12-2023",
         endDate: "17-03-2024",
-        status: "not done",
     }
 ];
 
+let myprojectsList=[];
+
 function mainCopyOnHtml() {
-    let id=0;
     const list = document.getElementById('displyList');
     const div = document.createElement('div');
     list.innerHTML = ''; // Clear the list before appending
-    todolist.forEach((elem) => {
+    todolist.forEach((elem,index) => {
+
+        let newdiv=document.createElement("div");
         let li = document.createElement('li');
-        li.innerHTML = `${elem.task} - ${elem.startDate} - ${elem.endDate} - ${elem.status} <input type="radio" name="radio${id}"> <input type="radio" name="radio${id++}">`;
-        list.append(li);
+        li.innerHTML = `${elem.task}   ${elem.startDate}  ${elem.endDate} \n <input type="checkbox" value="done" id=${index} onClick="deleteTask(${index})">`;
+        list.append(newdiv);
+
+        newdiv.classList.add('taskfont')
+
+        newdiv.append(li);
+
     });
+
+
 }
 
 mainCopyOnHtml();
 
-function addTask() {
+function addTaskopenModel() {
+
+    let oldItme = document.querySelector('.displyHeader');
+    let newitem  = document.querySelector('.myprojects');
+
+    oldItme.style.display='flex';
+    newitem.style.display= 'none';
+
     let elem = document.querySelector('.model');
     elem.style.display = 'flex';
 }
@@ -41,6 +56,9 @@ function closeModel(event) {
 }
 
 function addTasks() {
+
+    
+
     let task = document.getElementById('taskName').value;
     let startDate = document.getElementById('startDate').value;
     let endDate = document.getElementById('endDate').value;
@@ -49,7 +67,6 @@ function addTasks() {
         task: task,
         startDate: startDate,
         endDate: endDate,
-        status: "not done" // Default status
     };
 
     todolist.push(list);
@@ -66,4 +83,63 @@ function addTasks() {
     document.getElementById('taskName').value ='';
     document.getElementById('startDate').value='';
     document.getElementById('endDate').value='';
+}
+
+function deleteTask(index)
+{
+
+    addToMyProjects(index)
+    
+    todolist.splice(index,1);
+
+    mainCopyOnHtml();
+
+}
+
+function addToMyProjects(index)
+{
+
+    let item = todolist[index];
+    myprojectsList.push(item);
+
+    console.log("new array is:");
+
+    console.log(myprojectsList);
+
+}
+
+
+function showMyProjects()
+{
+    if (myprojectsList.length==0)
+        alert("No projects done")
+
+    let oldItme = document.querySelector('.displyHeader');
+    let newitem  = document.querySelector('.myprojects');
+
+    oldItme.style.display='none';
+    newitem.style.display= 'flex';
+
+    let projectList = document.getElementById('doneProject');
+
+    projectList.innerHTML="";
+
+    myprojectsList.forEach(elem =>{
+
+        let li = document.createElement('li');
+
+        let addDiv = document.createElement('div');
+
+        addDiv.className = "taskfont";
+
+        projectList.append(addDiv);
+
+        li.innerHTML = `${elem.task} End Date : ${elem.endDate}`;
+
+        addDiv.append(li);
+
+    })
+
+
+
 }
